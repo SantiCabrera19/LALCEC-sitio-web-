0d11d74d6e660e26df2f4a8d126c22055274d7a8
SantiCabrera19 <cabrerasanty321@gmail.com>
2022-08-12 01:54:43 -0300

    pain

--- Grupo2-ProyectoFinal-master/Django_prueba/settings/base.py
+++ Grupo2-ProyectoFinal-master/Django_prueba/settings/base.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4', # Acá
+    'bootstrap4',
+    'blogapp' # Acá
     ]
 
 MIDDLEWARE = [
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
--- Grupo2-ProyectoFinal-master/Django_prueba/urls.py
+++ Grupo2-ProyectoFinal-master/Django_prueba/urls.py
@@ -1,7 +1,7 @@
-"""Django_prueba URL Configuration
+"""boostrap_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from blogapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
