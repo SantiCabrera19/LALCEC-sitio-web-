43a40a88687deb3f40d1964b1032378a36aa5e52
SantiCabrera19 <cabrerasanty321@gmail.com>
2022-08-12 08:21:05 -0300

    Cambio de nombre

--- Grupo2-ProyectoFinal-master(abrircode)/Django_prueba/settings/base.py
+++ Grupo2-ProyectoFinal-master(abrircode)/Django_prueba/settings/base.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Django_prueba.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+   'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
