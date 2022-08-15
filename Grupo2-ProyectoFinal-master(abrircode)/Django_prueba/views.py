270806a0d6c97e55961922e3de7485acf71d4ccf
SantiCabrera19 <cabrerasanty321@gmail.com>
2022-08-15 18:40:14 -0300

    cambiando caracteristica

--- Grupo2-ProyectoFinal-master(abrircode)/Django_prueba/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, "blogapp/index.html")
-
-def cancer1(request):
-    return render(request, "blogapp/cancer1.html")
