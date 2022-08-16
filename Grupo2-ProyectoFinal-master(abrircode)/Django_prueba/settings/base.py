84817262831be468bdb5606d0c07f5b76e7dd08c
SantiCabrera19 <cabrerasanty321@gmail.com>
2022-08-16 16:55:13 -0300

    delete

--- Grupo2-ProyectoFinal-master(abrircode)/Django_prueba/settings/base.py
+++ Grupo2-ProyectoFinal-master(abrircode)/Django_prueba/settings/base.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Django_prueba.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backeds.postgresql_psycopg2',
+        'NAME' : 'mydb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' : 5432,
     }
 }
 
--- Grupo2-ProyectoFinal-master(abrircode)/blogapp/models.py
+++ Grupo2-ProyectoFinal-master(abrircode)/blogapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
 # Create your models here.
+
+class prueba(models.model):
+    title = models.CharField(max_lenght=50)
+    description = models.TextField(blank=True)
